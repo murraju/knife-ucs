1f283058cc0cb27517033c9330ae87684a6b0cb1
Murali Raju <murraju@cisco.com>
2012-05-31 17:05:26 -0400

    refactor

--- lib/chef/knife/ucs_pool_create.rb
+++ lib/chef/knife/ucs_pool_create.rb
@@ -30,14 +30,40 @@ class Chef
 
         option :pool,
           :long => "--pool POOLTYPE",
-          :description => "UCS pool types <mac,uuid,wwpn,wwnn,management>",
+          :description => "UCS pool types <mac,uuid,wwpn,wwnn,managementip>",
           :proc => Proc.new { |f| Chef::Config[:knife][:pool] = f }
 
-        option :poolname,
+        option :name,
           :long => "--poolname POOLNAME",
           :description => "The pool name",
-          :proc => Proc.new { |f| Chef::Config[:knife][:poolname] = f }
+          :proc => Proc.new { |f| Chef::Config[:knife][:name] = f }
 
+        option :start,
+          :long => "--poolstart STARTRANGE",
+          :description => "Start of a pool range <IP, WWPN, WWNN, MAC>",
+          :proc => Proc.new { |f| Chef::Config[:knife][:start] = f }
+
+        option :end,
+          :long => "--poolend ENDRANGE",
+          :description => "End of a pool range <IP, WWPN, WWNN, MAC>",
+          :proc => Proc.new { |f| Chef::Config[:knife][:end] = f }
+
+
+        option :mask,
+          :long => "--subnetmask SUBNETMASK",
+          :description => "The subnet mask for an IP range",
+          :proc => Proc.new { |f| Chef::Config[:knife][:mask] = f }
+
+        option :gateway,
+          :long => "--gateway IPGATEWAY",
+          :description => "The IP Gateway address of a subnet",
+          :proc => Proc.new { |f| Chef::Config[:knife][:gateway] = f }
+          
+        type = "#{Chef::Config[:knife][:pool]}"
+        case type
+        when 'managementip'
+          json = { :start_ip => Chef::Config[:knife][:start],  }
+        end
 
       end
 
