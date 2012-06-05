6b77029d6ef4bbc2fad8f6be3daf8948a5e083e2
Murali Raju <murali.raju@appliv.com>
2012-06-04 20:58:23 -0400

    refactor

--- lib/chef/knife/ucs_policy_create.rb
+++ lib/chef/knife/ucs_policy_create.rb
@@ -44,11 +44,63 @@ class Chef
         :description => "The policy name",
         :proc => Proc.new { |f| Chef::Config[:knife][:name] = f }
 
+      option :org,
+        :long => "--org ORG",
+        :description => "The organization to use",
+        :proc => Proc.new { |f| Chef::Config[:knife][:name] = f }
+
+      option :hardwaremodel,
+        :long => "--hardware-model MODEL",
+        :description => "The hardware model",
+        :proc => Proc.new { |f| Chef::Config[:knife][:hardwaremodel] = f }
+
+      option :hardwaretype,
+        :long => "--hardware-type TYPE",
+        :description => "The hardware type",
+        :proc => Proc.new { |f| Chef::Config[:knife][:hardwaretype] = f }
+
+      option :hardwarevendor,
+        :long => "--hardware-vendor VENDOR",
+        :description => "The hardware vendor",
+        :proc => Proc.new { |f| Chef::Config[:knife][:hardwarevendor] = f }
+
+      option :firmwareversion,
+        :long => "--firmware-version VERSION",
+        :description => "The firmware version",
+        :proc => Proc.new { |f| Chef::Config[:knife][:firmwareversion] = f }
+
       def run
         $stdout.sync = true
-             
+        
+        policy = "#{Chef::Config[:knife][:policy]}".downcase
+        case policy
+        when 'host-firmware'
+          json = {  :host_firmware_pkg_name => Chef::Config[:knife][:name], :hardware_model => Chef::Config[:knife][:hardwaremodel],
+                    :hardware_type => Chef::Config[:knife][:hardwaretype],  :hardware_vendor => Chef::Config[:knife][:hardware_vendor],
+                    :firwmare_version Chef::Config[:knife][:firwmare_version], :org => Chef::Config[:knife][:org]  }.to_json
+                    
+          puts provisioner.create_host_firmware_package(json)          
+
+          # xml_response = provisioner.create_host_firmware_package(json)
+          # xml_doc = Nokogiri::XML(xml_response)
+          # 
+          # xml_doc.xpath("configConfMos/outConfigs/pair/ippoolBlock").each do |ippool|
+          #   puts ''
+          #   puts "Management IP Block from: #{ui.color("#{ippool.attributes['from']}", :blue)} to: #{ui.color("#{ippool.attributes['to']}", :blue)}" + 
+          #         " status: #{ui.color("#{ippool.attributes['status']}", :green)}"
+          # end
+          # 
+          # #Ugly...refactor later to parse error with better exception handling. Nokogiri xpath search for elements might be an option
+          # xml_doc.xpath("configConfMos").each do |ippool|
+          #    puts "#{ippool.attributes['errorCode']} #{ui.color("#{ippool.attributes['errorDescr']}", :red)}"
+          # end
+          
+        else
+          puts "Incorrect options. Please make sure you are using one of the following: "
+        end     
         
       end
+      
     end
   end
 end
